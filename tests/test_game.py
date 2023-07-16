5b95b8e3a31bdf93f66c7b0702308164eb19a006
yurika kodama <m21053@g.metro-cit.ac.jp>
2023-07-16 18:37:38 +0900

    Game__init__ テスト

---
+++ tests/test_game.py
@@ -0,0 +1,34 @@
+from animalshougi.model import Game, Piece
+import pytest
+
+
+def test_init():
+    g = Game()
+
+    assert g.turn == 0
+    empty_hands = {
+        Piece.LION: 0,
+        Piece.GIRAFFE: 0,
+        Piece.ELEPHANT: 0,
+        Piece.CHICK: 0,
+    }
+    assert g.hands == (empty_hands, empty_hands)
+    assert g.table == [
+        [(False, Piece.GIRAFFE), (False, Piece.LION), (False, Piece.ELEPHANT)],
+        [None, (False, Piece.CHICK), None],
+        [None, (True, Piece.CHICK), None],
+        [(True, Piece.ELEPHANT), (True, Piece.LION), (True, Piece.GIRAFFE)],
+    ]
+
+    g = Game(f"1\n001000\ngle\n.C.\n...\nELG")
+
+    assert g.turn == 1
+    having_chick = empty_hands.copy()
+    having_chick[Piece.CHICK] = 1
+    assert g.hands == (having_chick, empty_hands)
+    assert g.table == [
+        [(False, Piece.GIRAFFE), (False, Piece.LION), (False, Piece.ELEPHANT)],
+        [None, None, None],
+        [None, (False, Piece.CHICK), None],
+        [(True, Piece.ELEPHANT), (True, Piece.LION), (True, Piece.GIRAFFE)],
+    ]
